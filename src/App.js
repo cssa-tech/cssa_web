d05fa9f817744f5c5c41bc643e84a8afe04eeaee
Yuang Fan <yuangf2@illinois.edu>
2024-10-01 17:33:54 -0500

    Create new event page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarHook from "./NavbarHook/NavbarHook";
 import Home from "./pages/Home";
 import Shiwu from "./pages/shiwu_page/shiwu";
@@ -9,6 +9,7 @@ import Business from "./pages/business_page/business";
 import About from "./pages/about/about";
 import Apply from "./pages/apply/apply";
 import Events from "./pages/events/events";
+import NewEvent from "./pages/new_event/new_event";
 import Resources from "./pages/resources/resources";
 
 const AppRoutes = () => {
@@ -17,10 +18,9 @@ const AppRoutes = () => {
 
   return (
     <div className="wrapper">
-    {/* <div className={`app-container ${isHomePage ? 'home-background' : ''}`}> */}
+      {/* <div className={`app-container ${isHomePage ? 'home-background' : ''}`}> */}
       <div className="app-container">
-        
-        <NavbarHook className="navbar"/> 
+        <NavbarHook className="navbar" />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -30,11 +30,11 @@ const AppRoutes = () => {
             <Route path="/about" element={<About />} />
             <Route path="/apply" element={<Apply />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/new_event" element={<NewEvent />} />
             <Route path="/resources" element={<Resources />} />
             {/* Define other routes that you need */}
           </Routes>
         </main>
-        
       </div>
     </div>
   );
---
+++ src/pages/new_event/new_event.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./new_event.css";
+import axios from "axios";
+
+const NewEvent = () => {
+  return (
+    <div>
+      <p>新建活动</p>
+    </div>
+  );
+};
+
+export default NewEvent;
